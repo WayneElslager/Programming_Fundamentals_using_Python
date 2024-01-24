73aa7305d147cf6563cfec4bd4e5d0c5afd58bb4
Wayne Elslager <WEE8@pitt.edu>
2024-01-24 14:02:01 -0500

    Adding to strings.py

--- strings.py
+++ strings.py
@@ -55,7 +55,6 @@ print(first_name + ' ' + last_name + ': ' + str(age))
 # String multiplication
 print("Hello"*3)
 # Can only add 2 strings and can only multiply a string with an int
-'''
 
 # Accessing string characters
 emp_name = "Jane Doe"
@@ -63,4 +62,31 @@ print(emp_name[3])
 # print(emp_name[8]) # throws index out of bounds error because the last char is at index 7
 
 print(emp_name.index('n'))
+'''
 
+# STRING SLICING
+emp_name = "Jane Doe"
+print(emp_name[2:6])
+print(emp_name[:4])
+print(emp_name[2:])
+print(emp_name[-4:-1])
+print(emp_name[1:6:2])
+# 2 counts as a step value, it is every other letter
+
+print(emp_name.count('e'))
+# Counts # of characters
+print(emp_name.find('Doe'))
+
+emp_name = (emp_name.replace('Jane', 'John'))
+
+print('oh' in emp_name)
+
+#String Formatting
+student_name = "Alice"
+score = 87
+print(student_name + ": " + str(score))
+print("Name: {} Score: {}".format(student_name, score))
+# f-strings
+
+print(f'Name: {student_name} Score: {score}')
+print(f'10 times 3 is {10*3}')
